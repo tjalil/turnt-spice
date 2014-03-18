911461e8c30323b382980f1c8990d0bb8270191b
Taha Jalil <taha.jalil@gmail.com>
2014-03-18 14:35:26 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RyanmingInstallerSampleapp::Application.config.secret_key_base = '05d18aa77a13197e63b7d1669cd9d2c27bfbde9590c16e8139c7f2e1eaf82540ff005434f25335e4d1196b1088ef5b75a9017305b7d555b4acc507c4424a2893'
